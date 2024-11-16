a723c3ab79ba12c32f72682608eb7a8b176beb9a
jnana890 <sahoojnana3002@gmail.com>
2024-11-16 22:43:04 +0530

    first setup commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->list[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='jnana',
+    author_email='sahoojnana3002@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
